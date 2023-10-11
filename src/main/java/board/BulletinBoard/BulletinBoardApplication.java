e698f972260b7ccf617c207bc573ff15cda36827
Kim minsu <ggeggrgg@naver.com>
2023-10-11 17:20:30 +0900

    Update BulletinBoardApplication.java

--- src/main/java/board/BulletinBoard/BulletinBoardApplication.java
+++ src/main/java/board/BulletinBoard/BulletinBoardApplication.java
@@ -2,8 +2,10 @@ package board.BulletinBoard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BulletinBoardApplication {
 
 	public static void main(String[] args) {

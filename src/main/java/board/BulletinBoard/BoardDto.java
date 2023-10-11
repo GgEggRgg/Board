8708bca72f2c0389eec0fa6005dab23945baea98
Kim minsu <ggeggrgg@naver.com>
2023-10-11 17:18:36 +0900

    Create BoardDto.java

---
+++ src/main/java/board/BulletinBoard/BoardDto.java
@@ -0,0 +1,40 @@
+package board.BulletinBoard;
+
+import board.BulletinBoard.domain.Board;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BoardDto {
+    private Long id;
+    private String writer;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public Board toEntity(){
+        Board board = Board.builder()
+                .id(id)
+                .writer(writer)
+                .title(title)
+                .content(content)
+                .build();
+        return board;
+    }
+
+    @Builder
+    public BoardDto(Long id, String title, String content, String writer,
+                    LocalDateTime createdDate, LocalDateTime modifiedDate){
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+}

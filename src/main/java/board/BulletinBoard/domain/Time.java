fdba20199b8021efbe2f4d3968621d31f5962de4
Kim minsu <ggeggrgg@naver.com>
2023-10-11 17:21:13 +0900

    Create Time.java

---
+++ src/main/java/board/BulletinBoard/domain/Time.java
@@ -0,0 +1,23 @@
+package board.BulletinBoard.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Time {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}

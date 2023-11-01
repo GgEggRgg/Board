42385dcf4def36ae41e2473cf24ddebf6b49d320
Kim minsu <ggeggrgg@naver.com>
2023-11-01 18:26:06 +0900

    FIx chatRoomdto to chatRoomdetailDto

--- src/main/java/board/BulletinBoard/service/ChatService.java
+++ src/main/java/board/BulletinBoard/service/ChatService.java
@@ -1,7 +1,7 @@
 package board.BulletinBoard.service;
 
+import board.BulletinBoard.domain.ChatMessage;
 import board.BulletinBoard.domain.ChatRoom;
-import board.BulletinBoard.domain.dto.ChatMessageDetailDto;
 import board.BulletinBoard.domain.dto.ChatRoomDetailDto;
 import board.BulletinBoard.domain.dto.ChatRoomDto;
 import board.BulletinBoard.repository.ChatRepository;
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -23,32 +22,33 @@ public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
 
     //해당 채팅룸의 메세지들 불러오기(채팅 리포)
-    public List<ChatMessageDetailDto> findAllChatByRoomId(String roomId) {
-        return chatRepository.findAllChatByChatRoomId(roomId);
+    public List<ChatMessage> findAllChatByRoomId(String roomId) {
+        ChatRoom chatRoom = chatRoomRepository.findByRoomId(roomId);
+        return chatRepository.findAllChatByChatRoomId(chatRoom.getId());
     }
 
     //채팅룸 목록 전체 조회하기(채팅방 리포)
-    public List<ChatRoomDto> getChatRoomList(){
-        List<ChatRoomDto> chatRoomList = new ArrayList<>();
+    public List<ChatRoomDetailDto> getChatRoomList(){
+        List<ChatRoomDetailDto> chatRoomList = new ArrayList<>();
         List<ChatRoom> rooms = chatRoomRepository.findAll();
         for (ChatRoom room : rooms) {
-            ChatRoomDto chatRoomDto = ChatRoomDto.create(
-                    room.getRoomName(), room.getCreatedBy());
-            chatRoomList.add(chatRoomDto);
+            ChatRoomDetailDto detail = ChatRoomDetailDto.toChatRoomDetailDto(room);
+            chatRoomList.add(detail);
         }
         return chatRoomList;
     }
 
     //채팅방 개설(채팅방 리포)
-    public Long saveChatRoom(ChatRoomDto chatRoomDto){
+    public String saveChatRoom(ChatRoomDto chatRoomDto){
         ChatRoom chatRoom = ChatRoom.toChatRoomEntity(
-                chatRoomDto.getRoomName(), chatRoomDto.getRoomId());
-        return chatRoomRepository.save(chatRoom).getId();
+                chatRoomDto.getRoomId(),
+                chatRoomDto.getRoomName(),
+                chatRoomDto.getCreatedBy());
+        return chatRoomRepository.save(chatRoom).getRoomId();
     }
 
-    public ChatRoomDetailDto findRoomById(Long id){
-        Optional<ChatRoom> chatRoomWrapper = chatRoomRepository.findById(id);
-        ChatRoom chatRoom = chatRoomWrapper.get();
+    public ChatRoomDetailDto findRoomByRoomId(String roomId){
+        ChatRoom chatRoom = chatRoomRepository.findByRoomId(roomId);
         ChatRoomDetailDto detail = ChatRoomDetailDto.toChatRoomDetailDto(chatRoom);
         return detail;
     }

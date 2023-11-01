2580fa45602d054d61e8e8eadd42cb451071ea70
Kim minsu <ggeggrgg@naver.com>
2023-11-01 18:26:43 +0900

    Fix Typo

--- src/main/java/board/BulletinBoard/domain/dto/ChatRoomDetailDto.java
+++ src/main/java/board/BulletinBoard/domain/dto/ChatRoomDetailDto.java
@@ -11,14 +11,14 @@ import lombok.NoArgsConstructor;
 public class ChatRoomDetailDto {
     private Long chatRoomId;
     private String roomId;
-    private String name;
+    private String roomName;
     private String createdBy;
 
     public static ChatRoomDetailDto toChatRoomDetailDto(ChatRoom chatRoom){
         ChatRoomDetailDto detailDto = new ChatRoomDetailDto();
         detailDto.setChatRoomId(chatRoom.getId());
         detailDto.setRoomId(chatRoom.getRoomId());
-        detailDto.setName(chatRoom.getRoomName());
+        detailDto.setRoomName(chatRoom.getRoomName());
         detailDto.setCreatedBy(chatRoom.getCreatedBy());
         return detailDto;
     }

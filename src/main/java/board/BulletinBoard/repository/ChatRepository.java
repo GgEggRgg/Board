a68b6d079b5d9610a4056ae7547292a6bf70a042
Kim minsu <ggeggrgg@naver.com>
2023-11-01 18:26:25 +0900

    Fix chatMessageDetailDto to chatMessage

--- src/main/java/board/BulletinBoard/repository/ChatRepository.java
+++ src/main/java/board/BulletinBoard/repository/ChatRepository.java
@@ -1,11 +1,10 @@
 package board.BulletinBoard.repository;
 
 import board.BulletinBoard.domain.ChatMessage;
-import board.BulletinBoard.domain.dto.ChatMessageDetailDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessageDetailDto> findAllChatByChatRoomId(String roomId);
+    List<ChatMessage> findAllChatByChatRoomId(Long id);
 }
